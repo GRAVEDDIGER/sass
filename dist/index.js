68d3b944a4e9e238084cb9fac9c3a8bae77477c3
Adrian Daniel Abadin <aabadin@gmail.com>
2022-04-19 09:33:23 -0300

    Segunda tarjeta

--- dist/index.js
+++ dist/index.js
@@ -184,12 +184,7 @@ function reloadCSS() {
 }
 
 module.exports = reloadCSS;
-},{"./bundle-url":"node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"scss/estilos.scss":[function(require,module,exports) {
-var reloadCSS = require('_css_loader');
-
-module.hot.dispose(reloadCSS);
-module.hot.accept(reloadCSS);
-},{"C:\\Users\\rsxab\\Desktop\\coder\\pruebas\\sass\\dist\\Gotham-Book.otf":[["Gotham-Book.b35de8b4.otf","dist/Gotham-Book.otf"],"dist/Gotham-Book.otf"],"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./bundle-url":"node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -394,4 +389,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
-//# sourceMappingURL=/estilos.1b0a6bcd.js.map
+//# sourceMappingURL=/index.js.map
